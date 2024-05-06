0d911cace26e83c8791cf356090cff328aa847e6
Jerry Chen <38683425+caffreygo@users.noreply.github.com>
2024-05-06 15:27:36 +0800

    Initial commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// import Versions from './components/Versions.vue'
+
+// const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <Suspense>
+    <RouterView />
+  </Suspense>
+</template>
